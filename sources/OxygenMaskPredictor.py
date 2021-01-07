4730dbe70552569d0e91d135937681faeb19b94e
Fabian Trottmann <fabian.trottmann@bbv.ch>
2021-01-07 19:03:33 +0100

    make predictions for latest image in certain folder

--- sources/OxygenMaskPredictor.py
+++ sources/OxygenMaskPredictor.py
@@ -1,11 +1,12 @@
 import os
 import numpy as np
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
 import skimage.io
 from mrcnn.config import Config
 from mrcnn.model import MaskRCNN
 import mrcnn.model as modellib
+import time
 
 class PredictionConfig(Config):
     NAME = "oxygenmask_cfg"
@@ -13,7 +14,7 @@ class PredictionConfig(Config):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
 
-def imgToArray(absoluteFilePath):
+def imgToArray(absoluteFilePath): # copied from MaskRCNN
     image = skimage.io.imread(absoluteFilePath)
     # If grayscale. Convert to RGB for consistency.
     if image.ndim != 3:
@@ -23,19 +24,16 @@ def imgToArray(absoluteFilePath):
         image = image[..., :3]
     return image
 
-def plot_prediction(absolutePathImage, model, cfg):
+def create_plot_prediction(absolutePathImage, model, cfg):
     print("predict masks for image: {0}".format(absolutePathImage))
-
     classNameById = {1: "with_mask", 2: "without_mask", 3: "mask_weared_incorrect"}
     image = imgToArray(absolutePathImage)
     scaled_image = modellib.mold_image(image, cfg)
     sample = np.expand_dims(scaled_image, 0)
     y_predict = model.detect(sample, verbose=0)[0]
-
-    pyplot.subplot(len(imageFiles), 2, i * 2 + 2)
-    pyplot.imshow(image)
-    pyplot.title('Oxygen Mask Prediction')
-    ax = pyplot.gca()
+    plt.imshow(image)
+    plt.title('Oxygen Mask Prediction')
+    ax = plt.gca()
     for i in range(len(y_predict['rois'])):
         y1, x1, y2, x2 = y_predict['rois'][i]
         width, height = x2 - x1, y2 - y1
@@ -44,25 +42,43 @@ def plot_prediction(absolutePathImage, model, cfg):
         print("className: {0}".format(className))
         rect = Rectangle((x1, y1), width, height, fill=False, color='red')
         ax.add_patch(rect) # draw box
-    pyplot.show()
+    return plt
+
+def init_model_config():
+    print("[x] to close")
+    config = PredictionConfig()
+    sourcePath = os.path.dirname(os.path.abspath(__file__))
+    model = MaskRCNN(mode='inference', model_dir=sourcePath, config=config)
+    model.load_weights('./mask_rcnn_oxygenmask_cfg_0005.h5', by_name=True)
+    return model, config
 
 def get_lastImage(imageBasePath):
+    imageFiles = [os.path.join(imageBasePath, fileName) for fileName in os.listdir(imageBasePath) if
+                  fileName.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp'))]
+    oldestImageFile = None
 
-    return imageBasePath
+    if len(imageFiles) > 0:
+        try:
+            oldestImageFile = max(imageFiles, key=os.path.getctime)
+        except:
+            print("image files have been removed")
 
-cfg = PredictionConfig()
+    return oldestImageFile
+
+model,config = init_model_config()
+
 sourcePath = os.path.dirname(os.path.abspath(__file__))
-
-model = MaskRCNN(mode='inference', model_dir=sourcePath, config=cfg)
-model.load_weights('mask_rcnn_oxygenmask_cfg_0005.h5', by_name=True)
-
-imageBasePath = sourcePath
+imageBasePath = os.path.join(sourcePath, "Predictions")
 imageFiles = [os.path.join(imageBasePath, fileName) for fileName in os.listdir(imageBasePath) if fileName.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp'))]
 
-print("[x] to close")
+lastImage = None
 while True:
-    absoluteFilePath = get_lastImage()
-    plot_prediction(absoluteFilePath, model, cfg)
-
-
-
+    currentImage = get_lastImage(imageBasePath)
+    if currentImage != None and lastImage != currentImage:
+        print(currentImage)
+        lastImage = currentImage
+        if plt != None:
+            plt.close()
+        plt = create_plot_prediction(currentImage, model, config)
+        plt.pause(2)
+        plt.show(block=False)
